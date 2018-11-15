137c35823f767e294ff1eac87c42333d856e65d0
J.S. Aguilar Loyo <j.s.aguilar@student.rug.nl>
2018-11-15 23:00:35 +0100

    first part

--- fre.py
+++ fre.py
@@ -7,22 +7,24 @@ Created on Sun Oct 14 13:55:09 2018
 
 
 import os
-os.chdir('C:\\Users\\s3179575\\LPR')
+os.chdir('C:\\Users\\s3179575.WORKSPACE\\LPR')
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.arima_model import ARIMA
 
-data = pd.read_table("sp500.csv",sep=",")
-data['Date'] = pd.to_datetime(data.Date,format='%Y-%m-%d')
+data = pd.read_table("sp500L.csv",sep=";")
+data['Date'] = pd.to_datetime(data.Date,format='%Y%m%d')
 data['year'] = data.Date.dt.year
-data = data[data['year'] <= 1984] 
+#data['Return_excD'] = data['Return_excD']*100 
+#data = data[data['year'] <= 1984] 
 
-data['Return'] = pd.DataFrame(((data.Close - data.Close.shift(1))/data.Close.shift(1))*100)  
-data['da'] = data.Date
-data['dat'] = pd.to_datetime(data.da,format='%Y-%m-%d')
-data['year'] = data.dat.dt.year
-data['mm'] = data.dat.dt.month
+#data['Return'] = pd.DataFrame(((data.Close - data.Close.shift(1))/data.Close.shift(1))*100)  
+#data['da'] = data.Date
+#data['dat'] = pd.to_datetime(data.da,format='%Y-%m-%d')
+#data['year'] = data.Date.dt.year
+data['mm'] = data.Date.dt.month
 
 #data_s = data_r.groupby(['year','mm'])['Close'].mean()        
 #data_s = data_r.groupby(['year','mm'])['Close'].agg(lambda x: x.sum())        
@@ -37,16 +39,36 @@ def var(a):
         
     return np.sqrt(np.sum(v) + 2*np.sum(c))
 
-data_s = pd.DataFrame(data.groupby(['year','mm'])['Return'].agg(var)).reset_index()
-data_s = data_s[data_s['year']>= 1953] 
+data_m = pd.DataFrame(data.groupby(['year','mm'])['Return_excD'].agg(var)).reset_index()
+data_m = data_m.rename(columns={'Return_excD':'volatility'})
+data_m['Date'] = pd.to_datetime(dict(year=data_m.year, month=data_m.mm,day=1))
+data_m['Ln_vol'] = np.log(data_m['volatility'])
+data_m['PC_vol'] = ((data_m.volatility - data_m.volatility.shift(1))/data_m.volatility.shift(1))*100  
 
-mean = data_s.Return.mean()
-std = data_s.Return.std()
+data_m = data_m[data_m['year'] <= 1984] 
+data_m = data_m[data_m['year'] >= 1928]
+
+#data_s = data_s[data_s['year']>= 1953] 
+
+mean = data_m.volatility.mean()
+std = data_m.volatility.std()
+
+mean_pc = data_m.PC_vol.mean()
 
 print('Mean value',mean)
 print('Standar deviation',std)
 
-plt.plot(data_s.index ,data_s.Return)
+plt.plot(data_m.Date ,data_m.volatility)
+
+model = ARIMA(data_m['Ln_vol'],order=(0,1,3))
+model_fit = model.fit(disp=0)
+print(model_fit.summary())
+
+data_m['fit'] = model_fit.predict(typ='levels')
+data_m['std_fit'] = np.exp(data_m['fit']  + 0.5*np.var(model_fit.resid))
+data_m['var_fit'] = np.exp(2*data_m['fit']  + 2*np.var(model_fit.resid))
+
+plt.plot(data_m.Date ,data_m.std_fit)
 
 # %%
 
@@ -60,13 +82,8 @@ data_t['select'] = data_t['year']*100 + data_t['month']
 
 data_t = data_t[data_t['year'] <= 1984]
 
-
 data_c = pd.merge(data,data_t,on='Date')
 
-e_mon = lambda x: (x-x.mean())   
-
-#data_c['average'] = data_c.groupby(['year_y'])['DTB3'].transform(max)
-#data_c['average'] = data_c.groupby(['year_y'])['DTB3'].transform(max)
 
 data_c['Date'].idxmax()
 #df.loc[df['Value'].idxmax()]
@@ -77,7 +94,7 @@ data_p =  data_c[data_c.groupby(['select'])['Date'].transform(max) == data_c['Da
 data_p = data_p.rename(columns={'DTB3':'st'})
 data_p = data_p.reindex(range(len(data_c)), method='bfill')
 
-data_c['tbil'] = data_p
+data_c['tbil'] = data_p 
 # test_df2.reset_index(name='maxvalue').to_string(index=False)
 
 
