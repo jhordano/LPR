ded5b78eef15ee966ca7ab7acc1bf0b56bb21a39
J.S. Aguilar Loyo <j.s.aguilar@student.rug.nl>
2018-11-18 20:10:16 +0100

    almost finished

--- fre.py
+++ fre.py
@@ -7,11 +7,12 @@ Created on Sun Oct 14 13:55:09 2018
 
 
 import os
-os.chdir('C:\\Users\\s3179575.WORKSPACE\\LPR')
+os.chdir('C:\\Users\\s3179575\\LPR')
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels
 import statsmodels.api as sm
 from statsmodels.tsa.arima_model import ARIMA
 
@@ -104,7 +105,7 @@ data_c = pd.merge(data,data_t,on='Date')
 #df.loc[df['Value'].idxmax()]
 
 #data_c.groupby(['select'])[['Date']].max().xs('DTB3',axis=1)
-data_bo_m =  data_c[data_c.groupby(['select'])['Date'].transform(max) == data_c['Date']]['DTB3']
+data_bo_m =  data_c[data_c.groupby(['select'])['Date'].transform(max) == data_c['Date']]['DTB3']/100
 data_bo_m = data_bo_m.rename(columns={'DTB3':'st'})
 data_bo_m = data_bo_m.reindex(range(len(data_c)), method='bfill')
 
@@ -123,7 +124,7 @@ data_crsp['crsp'] = data_crsp['Mkt-RF'] + data_crsp['RF']
 data_crsp = data_crsp.rename(columns={'Mkt-RF':'spread'})
 data_crsp['year'] = data_crsp.Date.dt.year
 
-data_RF = pd.DataFrame(data=np.array(data_crsp['RF']), index=data_crsp['Date'])
+data_RF = pd.DataFrame(data=np.array(data_crsp['RF'])/100, index=data_crsp['Date'])
 data_RF_d = data_RF.reindex(data['Date'],method='bfill')
 
 #mean_crsp = np.mean(data_crsp[(data_crsp['year']>=1928) & (data_crsp['year']<=1984)].crsp)
@@ -191,7 +192,6 @@ def cun_pmf(x, mu, theta, a, b, c_1 , c_2):
                 
     return -np.sum(loglik)
 
-# %%
 class arch_c(GenericLikelihoodModel):
     def __init__(self, endog, exog=None, **kwds):
         if exog is None:
@@ -220,7 +220,7 @@ class arch_c(GenericLikelihoodModel):
             
         return super(arch_c, self).fit(start_params=start_params, maxiter=maxiter, maxfun=maxfun, **kwds)
 
-model = arch_c(data['spread'])
+model = arch_c(data['spread']*100)
 results = model.fit()
 results.summary()
 
@@ -252,18 +252,122 @@ print(res_wls.summary())
 
 #X = np.concatenate((np.array(data_m['std_fit']).reshape(684,1) , np.ones( (len(data_m['std_fit']),1))), axis=1)
 X = np.concatenate(( np.ones( (len(data_m['std_fit']),1)) , np.array(data_m['std_fit']).reshape(684,1) ) , axis=1)
+#X =  np.array(data_m['std_fit']).reshape(684,1) 
 y = np.array(data_m['spread'])
-Om = np.diag(1/data_m['std_fit'])
+#Om = np.diag(1/data_m['std_fit'])
 
-beta = np.matmul( np.linalg.inv(np.matmul(np.matmul(np.transpose(X),Om),X)), np.matmul(np.matmul(np.transpose(X),Om),y) )
+#beta_w = np.matmul( np.linalg.inv(np.matmul(np.matmul(np.transpose(X),Om),X)), np.matmul(np.matmul(np.transpose(X),Om),y) )
+#beta = np.matmul( np.linalg.inv(np.matmul(np.transpose(X),X)), np.matmul(np.transpose(X),y) )
+
+model_s = sm.WLS(y, X, weights = 1/data_m['std_fit'])
+results_s = model_s.fit()
+print(results_s.summary())
+
       
 X_1 = np.concatenate(( np.ones( (len(data_m['std_fit']),1)) , np.array(data_m['std_fit']).reshape(684,1) , np.array(data_m['Unp_std']).reshape(684,1) ) , axis=1)
-beta_1 = np.matmul( np.linalg.inv(np.matmul(np.matmul(np.transpose(X_1),Om),X_1)), np.matmul(np.matmul(np.transpose(X_1),Om),y) )
+#beta_1 = np.matmul( np.linalg.inv(np.matmul(np.matmul(np.transpose(X_1),Om),X_1)), np.matmul(np.matmul(np.transpose(X_1),Om),y) )
+model_s_u = sm.WLS(y,X_1, weights = 1/data_m['std_fit'])
+results_s_u = model_s_u.fit()
+print(results_s_u.summary())
 
-
-
+# %%
 X_v = np.concatenate(( np.ones( (len(data_m['var_fit']),1)) , np.array(data_m['var_fit']).reshape(684,1) ) , axis=1)
+beta_v = np.matmul( np.linalg.inv(np.matmul(np.matmul(np.transpose(X_v),Om),X_v)), np.matmul(np.matmul(np.transpose(X_v),Om),y) )
 
-beta_v = np.matmul( np.linalg.inv(np.matmul(np.matmul(np.transpose(X),Om),X)), np.matmul(np.matmul(np.transpose(X),Om),y) )     
+model_v = sm.WLS(y, X_v, weights = 1/data_m['std_fit'])
+results_v = model_v.fit()
+print(results_v.summary())
+     
 X_1 = np.concatenate(( np.ones( (len(data_m['var_fit']),1)) , np.array(data_m['var_fit']).reshape(684,1) , np.array(data_m['Unp_var']).reshape(684,1) ) , axis=1)
 beta_v_1 = np.matmul( np.linalg.inv(np.matmul(np.matmul(np.transpose(X_1),Om),X_1)), np.matmul(np.matmul(np.transpose(X_1),Om),y) )
+
+model_v_u = sm.WLS(y, X_1, weights = 1/data_m['std_fit'])
+results_v_u = model_v_u.fit()
+print(results_v_u.summary())
+
+
+
+
+# %% Estimation of the GARCH-M model
+from arch.univariate import ConstantMean, GARCH
+def lik_g_m(x, mu,beta,theta, a, b, c_1, c_2):
+    # compute residuals
+    n = len(x)
+    e = np.zeros(n)    
+    ss = np.sum(e*e)/(n-4)
+    # generate sigt and log likelihood
+    sigt = np.zeros(n)  ;
+    loglik = np.zeros(n);
+
+    for i in range(n):
+        if i <= 1:
+            sigt[i] = a + b*ss + c_1 + c_2;
+            e[i] = x[i] - mu - theta*ss 
+        else:
+            sigt[i] = a + b*sigt[i-1] + c_1*e[i-1]*e[i-1] + c_2*e[i-2]*e[i-2] ;
+            e[i] = x[i] - mu - beta*np.sqrt(sigt[i]) + theta*e[i-1]    
+            
+        loglik[i] = -0.5*(np.log(2*math.pi) + np.log(sigt[i]) + (e[i]*e[i])/sigt[i]);
+                
+    return -np.sum(loglik)
+
+class garch_m(GenericLikelihoodModel):
+    def __init__(self, endog, exog=None, **kwds):
+        if exog is None:
+            exog = np.zeros_like(endog)
+            
+        super(garch_m, self).__init__(endog, exog, **kwds)
+     
+    
+    def nloglikeobs(self, params):
+        mu = params[0]
+        beta = params[1]
+        theta = params[2]
+        a = params[3]
+        b = params[4]
+        c_1 = params[5]
+        c_2 = params[6]
+
+        return lik_g_m(self.endog,mu,beta,theta,a,b,c_1,c_2)
+    
+    def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
+        self.exog_names.append('beta')
+        self.exog_names.append('theta')
+        self.exog_names.append('a')
+        self.exog_names.append('b')
+        self.exog_names.append('c_1')
+        self.exog_names.append('c_2')
+        
+        gar_0 = ConstantMean(data['spread'])
+        gar_0.volatility = GARCH(p=2, q=1)
+        gar_0_r = gar_0.fit()
+        gar_pa_0 = np.array(gar_0_r.params)
+        sigma_2 = gar_0_r.conditional_volatility
+        
+        mean_0 = statsmodels.tsa.arima_model.ARMA(data['spread'],exog=sigma_2,order=(0,1))
+        mean_0_r = mean_0.fit()
+        mean_pa_0 = np.array(mean_0_r.params)        
+        
+        start_params = np.concatenate([ mean_pa_0 , [gar_pa_0[1]] , [gar_pa_0[4]] , gar_pa_0[2:4]])
+            
+        return super(garch_m, self).fit(start_params=start_params, maxiter=maxiter, maxfun=maxfun, **kwds)
+
+model_garch_m = garch_m(data['spread']*100)
+results_g_m = model_garch_m.fit()
+results_g_m.summary()
+
+
+# %%
+
+from arch.univariate import ConstantMean, GARCH
+gar_0 = ConstantMean(data['spread'])
+gar_0.volatility = GARCH(p=2, q=1)
+gar_0_r = gar_0.fit()
+gar_pa_0 = np.array(gar_0_r.params)
+sigma_2 = gar_0_r.conditional_volatility
+
+X = sm.add_constant(sigma_2)         
+#mean_0 = sm.tsa.ARMA(data['spread'], order=(0,1))
+mean_0 = statsmodels.tsa.arima_model.ARMA(data['spread'],exog=sigma_2,order=(0,1))
+mean_0_r = mean_0.fit()
+gar_pa_0 = np.array(mean_0_r.params)
