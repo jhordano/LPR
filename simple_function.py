7017726c1539dc8de018005994cc5c6d9867140a
J.S. Aguilar Loyo <j.s.aguilar@student.rug.nl>
2018-11-25 21:10:21 +0100

    unfeasibility of the variance

--- simple_function.py
+++ simple_function.py
@@ -177,3 +177,141 @@ class Derivative(object):
         return (f(x+h) - f(x))/h   
 
 
+
+# %%
+import scipy.stats as sc
+
+std = 4   
+mean = 1.5
+
+x_1 = 1
+print(sc.norm(mean, std).pdf(x_1))    
+
+pro = (1/(np.sqrt(2*math.pi*np.power(std,2))))*np.exp(-0.5*np.power((x_1-mean)/std,2))
+print(pro)
+
+
+# %%    
+import math
+from statsmodels.base.model import GenericLikelihoodModel
+import time
+import numpy as np
+N = 500
+x = np.ones(N)
+std = np.exp( 1.8*(sc.norm(1,0.1).rvs(size=N)) )
+
+y = x*2 + 3.5*np.power(std,2) + sc.norm(0,std).rvs(size=N)
+#y = x*3.2 + 2*std + sc.norm(0,std).rvs(size=N)
+
+def cun_pmf(x, mu, sig ,beta):
+    # compute residuals
+    n = len(x)
+    e = np.zeros(n)    
+    ss = np.sum(e*e)/(n-2)
+    # generate sigt and log likelihood
+    sigt = np.zeros(n);
+    loglik = np.zeros(n);
+
+    for i in range(n):
+#        if i <= 1:
+            sigt[i] = np.exp(2*sig);
+            e[i] = x[i] - mu - beta*np.sqrt(sigt[i]) 
+            
+#            e[i] = x[i] - mu - beta*np.power(sigt[i],2)             
+#        else:
+            #sigt[i] = a + b*sigt[i-1] + c_1*e[i-1]*e[i-1] + c_2*e[i-2]*e[i-2];
+#            sigt[i] = 
+#            e[i] = x[i] - mu
+            
+ #           loglik[i]  = np.log( sc.norm(0, np.sqrt(sigt[i])).pdf(e[i]))
+#            loglik[i] = -0.5*(np.log(2*math.pi) + np.log(np.power( sigt[i],2) ) + (e[i]*e[i])/(np.power( sigt[i],2) ));
+ 
+#            loglik[i] = -0.5*(np.log(2*math.pi) + np.log(sigt[i]) + np.power(x[i]-mu-1,2) -(2*(x[i]-mu)+np.power(x[i]-mu,2))*(sigt[i]-1) -(2*(x[i]-mu))*(beta));                 
+            loglik[i] = -0.5*(np.log(2*math.pi) + np.log(sigt[i]) + (e[i]*e[i])/( sigt[i] ));                
+            
+    return -np.sum(loglik)
+
+class arch_c(GenericLikelihoodModel):
+    def __init__(self, endog, exog=None, **kwds):
+        if exog is None:
+            exog = np.zeros_like(endog)
+            
+        super(arch_c, self).__init__(endog, exog, **kwds)
+        
+    def nloglikeobs(self, params):
+        mu = params[0]
+        sig = params[1]       
+        beta = params[2]        
+        
+        return cun_pmf(self.endog,mu,sig,beta)
+    
+    def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
+        start_params = np.array([2.8, 1.05, 1.8])
+            
+        return super(arch_c, self).fit(start_params=start_params, maxiter=maxiter, maxfun=maxfun, **kwds)
+
+
+model = arch_c(y)
+model_fit = model.fit()
+print(model_fit.summary())
+
+# %%
+
+def cun_1(x, mu, sig ,beta):
+    # compute residuals
+    n = len(x)
+    e = np.zeros(n)    
+    ss = np.sum(e*e)/(n-2)
+    # generate sigt and log likelihood
+    sigt = np.zeros(n);
+    loglik = np.zeros(n);
+
+    for i in range(n):
+#        if i <= 1:
+            sigt[i] = sig;
+            e[i] = x[i] - mu - beta*np.sqrt(sigt[i]) 
+            
+#            e[i] = x[i] - mu - beta*np.power(sigt[i],2)             
+#        else:
+            #sigt[i] = a + b*sigt[i-1] + c_1*e[i-1]*e[i-1] + c_2*e[i-2]*e[i-2];
+#            sigt[i] = 
+#            e[i] = x[i] - mu
+            
+ #           loglik[i]  = np.log( sc.norm(0, np.sqrt(sigt[i])).pdf(e[i]))
+#            loglik[i] = -0.5*(np.log(2*math.pi) + np.log(np.power( sigt[i],2) ) + (e[i]*e[i])/(np.power( sigt[i],2) ));
+ 
+#            loglik[i] = -0.5*(np.log(2*math.pi) + np.log(sigt[i]) + np.power(x[i]-mu-1,2) -(2*(x[i]-mu)+np.power(x[i]-mu,2))*(sigt[i]-1) -(2*(x[i]-mu))*(beta));                 
+            loglik[i] = -0.5*(np.log(2*math.pi) + np.log(sigt[i]) + (e[i]*e[i])/( sigt[i] ));                
+            
+    return -np.sum(loglik)
+
+class arch_c1(GenericLikelihoodModel):
+    def __init__(self, endog, exog=None, **kwds):
+        if exog is None:
+            exog = np.zeros_like(endog)
+            
+        super(arch_c1, self).__init__(endog, exog, **kwds)
+        
+    def nloglikeobs(self, params):
+        mu = params[0]
+        sig = params[1]       
+        beta = params[2]        
+        
+        return cun_1(self.endog,mu,sig,beta)
+    
+    def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
+        start_params = np.array([2.8, 1.05, 1.8])
+            
+        return super(arch_c1, self).fit(start_params=start_params, maxiter=maxiter, maxfun=maxfun, **kwds)
+
+
+t = time.time()
+
+y_s = y/np.exp(2)
+
+model = arch_c1(y_s)
+model_fit = model.fit()
+print(model_fit.summary())
+
+elapsed = time.time() - t
+# 0.24
